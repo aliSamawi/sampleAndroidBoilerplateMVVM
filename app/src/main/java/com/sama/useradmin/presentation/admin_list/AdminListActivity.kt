3b0f2409a40167bac893cb296ecfd733712e7e1c
ali samawi <alisamawy1370@gmail.com>
2020-06-12 23:33:30 +0430

    add profile route to list activity

--- app/src/main/java/com/sama/useradmin/presentation/admin_list/AdminListActivity.kt
+++ app/src/main/java/com/sama/useradmin/presentation/admin_list/AdminListActivity.kt
@@ -1,12 +1,17 @@
 package com.sama.useradmin.presentation.admin_list
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sama.useradmin.R
 import com.sama.useradmin.data.model.User
 import com.sama.useradmin.presentation.base.BaseActivity
+import com.sama.useradmin.presentation.profile.ProfileActivity
 import kotlinx.android.synthetic.main.activity_admin_list.*
+
 
 class AdminListActivity : BaseActivity<AdminListViewModel>() {
 
@@ -40,5 +45,18 @@ class AdminListActivity : BaseActivity<AdminListViewModel>() {
         viewModel.getRegularUsers().observe(this, Observer {
             userAdapter.updateItems(it)
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.admin_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id: Int = item.itemId
+        return if (id == R.id.profile) {
+            startActivity(Intent(this, ProfileActivity::class.java))
+            true
+        } else super.onOptionsItemSelected(item)
     }
 }
