20140766cb147e770fb9b1d180cd49f42613a8f0
ali samawi <alisamawy1370@gmail.com>
2020-06-12 14:44:55 +0430

    impl validation of signup

--- app/src/main/java/com/sama/useradmin/presentation/signup/SignUpViewModel.kt
+++ app/src/main/java/com/sama/useradmin/presentation/signup/SignUpViewModel.kt
@@ -1,7 +1,51 @@
 package com.sama.useradmin.presentation.signup
 
+import android.util.Patterns
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sama.useradmin.data.repository.Repository
+import java.util.regex.Pattern
 import javax.inject.Inject
 
-class SignUpViewModel @Inject constructor(): ViewModel(){
+class SignUpViewModel @Inject constructor(
+    val repository: Repository
+): ViewModel(){
+
+    val validationError = MutableLiveData<String>()
+
+    fun signUpUser(fullname:String, email: String, password:String, confirmPassword:String){
+        if (fullname.isEmpty()){
+            validationError.value = "fullname should not be empty"
+            return
+        }
+
+        if (email.isEmpty()){
+            validationError.value = "email should not be empty"
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            validationError.value = "please enter valid email address"
+            return
+        }
+
+        if (password.isEmpty()){
+            validationError.value = "password should not be empty"
+            return
+        }
+        if (!Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$").matcher(password).matches()){
+            validationError.value = "Password field must contain numerical and capital values"
+            return
+        }
+
+        if (confirmPassword.isEmpty()){
+            validationError.value = "confirm password should not be empty"
+            return
+        }
+        if (confirmPassword != password){
+            validationError.value = "confirm password should not be different with password."
+            return
+        }
+
+
+    }
 }
